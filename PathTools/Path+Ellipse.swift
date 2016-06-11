d8c277b5470c1e63695a1c2e539d6232221bb86b
jsbean <jbean46@gmail.com>
2016-06-11 19:50:38 +0200

    Introduce Path.ellipse

---
+++ PathTools/Path+Ellipse.swift
@@ -0,0 +1,22 @@
+//
+//  Path+Ellipse.swift
+//  PathTools
+//
+//  Created by James Bean on 6/11/16.
+//
+//
+
+import QuartzCore
+
+extension Path {
+    
+    /**
+     - warning: Not yet implemented!
+     
+     - returns: `Path` with an ellipse shape within the given `rectangle`.
+     */
+    public static func ellipse(rectangle rectangle: CGRect) -> Path {
+        
+        return Path([])
+    }
+}
