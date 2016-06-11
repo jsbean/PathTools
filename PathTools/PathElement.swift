120afcf3a29d28ba617ab2e1c53bc3cebc41339f
jsbean <jbean46@gmail.com>
2016-06-12 00:32:59 +0200

    Add doc comments

--- PathTools/PathElement.swift
+++ PathTools/PathElement.swift
@@ -8,13 +8,39 @@
 
 import QuartzCore
 
+/**
+ Wrapper fro `CGPath` `PathElement.type`.
+ */
 public enum PathElement {
+    
+    /**
+     Move to point.
+     */
     case move(CGPoint)
+    
+    /**
+     Add line to point.
+     */
     case line(CGPoint)
+    
+    /**
+     Add quadratic bézier curve to point, with control point.
+     */
     case quadCurve(CGPoint, CGPoint)
+    
+    /**
+     Add cubic bézier curve to point, with two control points.
+     */
     case curve(CGPoint, CGPoint, CGPoint)
+    
+    /**
+     Close subpath.
+     */
     case close
     
+    /**
+     Create a `PathElement` with a `CGPathElement`.
+     */
     public init(element: CGPathElement) {
         switch element.type {
         case .MoveToPoint:
